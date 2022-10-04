66cc2f95f62b1b7fbe489f38714ae8b13571eee4
Didenchuk <tdidenchuk@gmail.com>
2022-10-04 21:29:12 +0200

    Change content to the designed

--- src/Weather.js
+++ src/Weather.js
@@ -1,60 +1,70 @@
-import React, {useState} from "react";
-import axios from "axios";
+import React from "react";
 import "./Weather.css";
 
-
-
-    
-    export default function Weather() {
-        let [city, setCity] = useState("");
-        let [loaded, setLoaded] = useState(false);
-        let [weather, setWeather] = useState("");
-      
-        function showWeather(responce) {
-          setLoaded(true);
-          setWeather({
-            temperature: responce.data.main.temp,
-            wind: responce.data.wind.speed,
-            humidity: responce.data.main.humidity,
-            icon: `http://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`,
-            description: responce.data.weather[0].description
-          });
-        }
-      
-        function handleSubmit(event) {
-          event.preventDefault();
-          let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d50daaabbd98c5b47ef6ff59824a0d1e&units=metric`;
-          axios.get(url).then(showWeather);
-        }
-      
-        function changeCity(event) {
-          setCity(event.target.value);
-        }
-        let form = (
-          <form onSubmit={handleSubmit}>
-            <input type="search" placeholder="Type a city" onChange={changeCity} />
-            <button type="submit"> Search </button>
-          </form>
-        );
-      
-        if (loaded) {
-          return (
-            <div>
-              {form}
-              <ul>
-                <li>Temperature: {Math.round(weather.temperature)}°C</li>
-                <li>Descrition: {weather.description}</li>
-                <li>Humidity: {weather.humidity} %</li>
-                <li>Wind: {weather.wind} km/h</li>
-                <li>
-                  {" "}
-                  <img src={weather.icon} alt={weather.description} />
-                </li>
-              </ul>
+export default function Weather() {
+  let weatherData = {
+    city: "Tokyo",
+    date: "Tuesday, 10:40",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/rain_heavy.png",
+    humidity: "80",
+    wind: "4",
+    temperature: "19"
+  };
+  return (
+    <div className="weather-app-wraper">
+      <div className="Weather">
+        <form className="mb-3">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city..."
+                className="form-control"
+                autocomplete="off"
+              />
             </div>
-          );
-        } else {
-          return form;
-        }
-      }
-      
+            <div className=" col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <div className=" overview">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li> Last updated: {weatherData.date}</li>
+            <li> {weatherData.description}</li>
+          </ul>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-start"
+              />
+              <div className="float-start">
+                <strong>{weatherData.temperature}</strong>
+                <span className="units">
+                  <a href="#">°C</a> | <a href="#">°F</a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className=" col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity} %</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
+        <div className="weather-forecast"></div>
+      </div>
+    </div>
+  );
+}
